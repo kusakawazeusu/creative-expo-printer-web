49b03635ea11839af6dc27b02ec966dc78e594af
kuan ting <kusakawazeusu@gmail.com>
2025-07-14 21:56:04 +0800

    fix: remove "prelod"

--- fonts/fonts.js
+++ fonts/fonts.js
@@ -2,13 +2,16 @@ import localFont from "next/font/local";
 import { Noto_Sans_TC } from "next/font/google";
 
 export const JinHeiFont = localFont({
+  preload: true,
   src: "../public/fonts/JinHeiFont.ttf",
 });
 
 export const Shrikhand = localFont({
+  preload: true,
   src: "../public/fonts/Shrikhand-Regular.ttf",
 });
 
 export const NotoSans = Noto_Sans_TC({
+  preload: false,
   weight: ["400", "500", "700"],
 });
